6921c41efe60abc78506314732f8f87b3d4da62d
pasca-l <shion.yamadate@keio.jp>
2022-06-10 19:54:54 +0900

    implemented to import torchvision defined models

--- scr/models.py
+++ scr/models.py
@@ -1,9 +1,13 @@
 import torch.nn as nn
+# https://pytorch.org/vision/stable/models.html
+import torchvision.models as models
 
 
 class AllergyClassifierModel(nn.Module):
     def __init__(self):
         super().__init__()
+        convnext = models.convnext_large(pretrained=True)
+        self.convnext = nn.Sequential(*list(convnext.children())[:])
 
     def forward(self, x):
-        return self(x)
+        return self.convnext(x)
