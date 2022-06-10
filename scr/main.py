03bb2115e556fac49c68fb2d4e1f7ae353dae398
pasca-l <shion.yamadate@keio.jp>
2022-06-10 20:13:55 +0900

    updated main

--- scr/main.py
+++ scr/main.py
@@ -4,19 +4,16 @@ from models import AllergyClassifierModel
 from system import AllergyClassifier
 from datasets import FoodDataModule
 
-# https://pytorch.org/vision/stable/models.html
-import torchvision.models as models
-
 
 def main():
     dataset = FoodDataModule(
                 data_dir='../food-101/images/',
                 ann_dir='../food-101/meta/')
-    model = models.convnext_large(pretrained=True)
+    model = AllergyClassifierModel()
     classifier = AllergyClassifier(model)
 
-    # trainer = pl.Trainer(max_epochs=1)
-    # trainer.fit(classifier, dataset)
+    trainer = pl.Trainer(default_root_dir='../logs/', max_epochs=1)
+    trainer.fit(classifier, dataset)
 
 
 if __name__ == '__main__':
