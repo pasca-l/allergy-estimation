c825521f27a5b4435a542a315c65099de70e7bef
pasca-l <shion.yamadate@keio.jp>
2022-06-14 13:04:53 +0000

    removed unnecessary libraries

--- src/datasets.py
+++ src/datasets.py
@@ -1,9 +1,7 @@
 import os
-import torch
 from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision import transforms
 import pytorch_lightning as pl
-import numpy as np
 from PIL import Image
 
 
