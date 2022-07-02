869f6d176841f3225e4f62af94cc021911ffc86a
pasca-l <shion.yamadate@keio.jp>
2022-07-02 13:35:27 +0000

    removed pandas, heapq, and cv2 dependencies

--- src/app_prediction.py
+++ src/app_prediction.py
@@ -1,8 +1,5 @@
-import torch
 import numpy as np
-import pandas as pd
-import heapq
-import cv2
+import torch
 from torchvision import transforms
 
 from datasets import FoodImageTransform
@@ -11,77 +8,39 @@ from system import AllergyClassifier
 
 
 class Predictor():
-    def __init__(self):
-        self.weight_file = "../food-101/meta/weights.csv"
-        self.model = AllergyClassifierModel(
-            weight_file=self.weight_file
-        )
+    def __init__(self,
+        weight_file='../data/meta/weights.csv',
+        ckpt_file='../logs/model.ckpt'
+    ):
+        self.model = AllergyClassifierModel()
         self.classifier = AllergyClassifier(self.model)
-        self.ckpt = torch.load("../logs/epoch=0-step=1263-v1.ckpt")
+        if torch.cuda.is_available():
+            self.ckpt = torch.load(ckpt_file)
+        else:
+            self.ckpt = torch.load(ckpt_file, map_location=torch.device('cpu'))
         self.classifier.load_state_dict(self.ckpt['state_dict'])
         self.classifier.eval()
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225]),
-        ])
-        self.table = pd.read_csv(self.weight_file)
-        self.allergen_list = np.array(self.table.columns)[1:]
-        self.food_list = np.array(self.table["table"])
-        self.weights = torch.tensor(np.loadtxt(self.weight_file, delimiter=',', skiprows=1, 
-                usecols=range(1, 28), dtype='float32'))
 
-    def index_list(self, l, x):
-        return [i for i, _x in enumerate(l) if _x == x]
+        self.transform = FoodImageTransform()
+        self.allergen_list = np.loadtxt(weight_file, delimiter=',', 
+                                        dtype='unicode')[:1,1:][0]
+        self.food_list = np.loadtxt(weight_file, delimiter=',', skiprows=1,
+                                    usecols=[0], dtype='unicode')
+        self.weights = np.loadtxt(weight_file, delimiter=',', skiprows=1, 
+                                  usecols=range(1, 28), dtype='float32')
 
-    def load_sample_img(self):
-        # img_path = "../img/sample_food.jpeg"
-        img_path = "../img/36641.jpg"
-        img = cv2.imread(img_path)
-        return img
-
-    def list_sort(self, output, hoge_list):
-        sorted_output = np.sort(output)[::-1]
-
-        sorted_list = hoge_list[np.argsort(output)][::-1]
-        possible_list = []
-        for ind in range(len(output)):
-            possible_list.append([np.argsort(output)[ind], sorted_list[ind], sorted_output[ind]])
-        return possible_list
-
-
-    def predict(self, img, output="a", debug=False):
-        if debug:
-            img = self.load_sample_img()
+    def predict(self, img):
         img = self.transform(img)
 
         with torch.no_grad():
-            if output == "a":
-                output_a = self.classifier(img.unsqueeze(0))
-                
-            elif output == "f":
-                output_f = self.classifier.model.forward_demo(img.unsqueeze(0))
-                output_a = torch.mm(output_f, torch.tensor(self.weights))
-            else:
-                print("output should be \"a\" or \"f\"")
-                return
+            food_prob = self.classifier(img.unsqueeze(0))
 
-        # n = 3
+        ordered_food_name = self.food_list[np.argsort(food_prob)]
+        ordered_food_prob = np.sort(food_prob)
 
-        if output == "a":
-            output_a = output_a.tolist()[0]
-            possible_allergen_list = self.list_sort(output_a, self.allergen_list)
-            print(possible_allergen_list)
-            return possible_allergen_list
+        allergen_prob = torch.mm(food_prob, torch.tensor(self.weights))
+        ordered_allergen_name = self.allergen_list[np.argsort(allergen_prob)]
+        ordered_allergen_prob = np.sort(allergen_prob)
 
-        elif output == "f":
-            output_f = output_f.tolist()[0]
-            output_a = output_a.tolist()[0]
-            possible_foods_list = self.list_sort(output_f, self.food_list)
-            possible_allergen_list = self.list_sort(output_a, self.allergen_list)
-            print(possible_foods_list[:5])
-            print(possible_allergen_list[:5])
-            return possible_foods_list, possible_allergen_list
+        return (ordered_food_name[:,:,-1][0], ordered_allergen_name[:,:,-1],
+                ordered_allergen_prob[:,:,-1])
